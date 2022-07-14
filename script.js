3a99f7a3d82d514e14ecadbbbed14243158c2a8b
imandato <imandato211@gmail.com>
2022-07-13 20:59:29 -0400

    created 3x3 table with border for mini sudoku board

---
+++ script.js
@@ -0,0 +1,10 @@
+// let solution = [[385],[261],[847]]
+
+
+// // From Java T Point: https://www.javatpoint.com/javascript-editable-table 
+// // This code uses arrays & array iterators to create a table that can be edited by the user 
+
+// let table = document.querySelector('table');
+// let dataCells = document.querySelectorAll('#table td');
+// let rows = document.querySelectorAll('tr');
+// rows = Array.prototype.slice.call(rows,1);
