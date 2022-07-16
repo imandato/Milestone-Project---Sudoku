350eb5ac7623039eaea4d11322ecc528d0465a5f
imandato <imandato211@gmail.com>
2022-07-16 09:50:55 -0400

    added javascript to retrieve input and add to an array

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 // let solution = [[385],[261],[847]]
 
 
-// // From Java T Point: https://www.javatpoint.com/javascript-editable-table 
-// // This code uses arrays & array iterators to create a table that can be edited by the user 
+// From Java T Point: https://www.javatpoint.com/javascript-editable-table 
+// This code uses arrays & array iterators to create a table that can be edited by the user 
 
 // let table = document.querySelector('table');
 // let dataCells = document.querySelectorAll('#table td');
 // let rows = document.querySelectorAll('tr');
 // rows = Array.prototype.slice.call(rows,1);
+// let initialData = {
+//     0: [1,2,3,4,5],
+//     1: [6,7,8,9,10],
+//     2: [11,12,13,14,15]
+// };
+
+// function parseTable(){
+//     var d = {};
+//     Array.prototype.forEach.call(rows, function(row,i){
+//         var rowrowCells = row.querySelectorAll('td');
+//         return Array.prototype.map.call(rowCells, function(cell,j){
+//             if (!d[j]) d[j] = [];
+//             d[j].push(parseInt(cell.textContent,10));
+//         });
+//     });
+//     return d;
+// }
+
+
+
+
+
+
+// function getValue(){
+//     this.value = parseFloat(this.value).toFixed(1);
+//     let inputValue = document.getElementById('input-1').value
+//     inputArray.push(inputValue)
+//     console.log(inputValue)
+// }
+
+
